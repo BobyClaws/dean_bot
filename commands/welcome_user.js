44d750e757fd72f87292f8471711fc704ad1ab0c
Boby <bobyclaws@yandex.com>
2020-11-08 01:15:11 +0530

    fix welcome user triggering for all servers

--- commands/welcome_user.js
+++ commands/welcome_user.js
@@ -1,16 +1,23 @@
 const Discord = require('discord.js');
-
+/**
+ * 
+ * @param {Discord.Client} client 
+ * @param {Discord.GuildMember} member 
+ */
 module.exports = function WelcomeUser(client, member) {
     console.log('new user joined');
-    //et channel = client.channels.cache.get('770156894077059083'); // boby's channel id
-    let channel = client.channels.cache.get('744209394358812726'); // general channel id
-    const embed = new Discord.MessageEmbed()
-        .setTitle('Welcome!')
-        .setDescription(`welcome to ${member.guild.name}, <@${member.id}>.\nHope you enjoy your stay!`)
-        .setImage('https://cdn.discordapp.com/attachments/744255255738318888/771421191147094056/welcome.gif')
-        .setThumbnail(member.guild.iconURL())
-        .setColor('#40ab40');
+    if (member.guild.name == 'Weebs ● パブ') {
+        let channel = client.channels.cache.get('744209394358812726'); // general channel id
+        const embed = new Discord.MessageEmbed()
+            .setTitle('Welcome!')
+            .setDescription(`welcome to ${member.guild.name}, <@${member.id}>.\nHope you enjoy your stay!`)
+            .setImage('https://cdn.discordapp.com/attachments/744255255738318888/771421191147094056/welcome.gif')
+            .setThumbnail(member.guild.iconURL())
+            .setColor('#40ab40');
+        
+        channel.send(embed);
+    }
+    // let channel = client.channels.cache.get('770156894077059083'); // boby's channel id
     
-    channel.send(embed);    
 }
 

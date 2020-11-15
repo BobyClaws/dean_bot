8c7b4249eba3a58579a1e87ba4d35b7fe73ffee3
Boby <liyakhath3s@gmail.com>
2020-11-16 02:35:59 +0530

    Update webhook_speak.js

--- commands/webhook_speak.js
+++ commands/webhook_speak.js
@@ -24,5 +24,32 @@ module.exports = (dclient,rclient) => {
 	  
     	}
     })
+
+
+        // // ghost-speak
+        // if(msg.content.startsWith("!wh")) {
+
+        //     args = evenSpace(msg.content).split(' ')
+            
+        //     console.log(args);
+            
+        //     msg.channel.createWebhook('ghost-speak', {
+        //         avatar: 'https://filebin.net/rv2ygpify0kavmra/willyou.jpg?t=ne3rlqve',
+        //     })
+        //         .then(webhook => console.log(`Created webhook ${webhook}`))
+        //         .catch(console.error);
+        //     let webhook = await msg.client.fetchWebhook('775804648752807948');
+           
+        //     await webhook.edit({
+        //         channel: args[1],
+        //         avatar: args[2],
+        //         name: args[3]
+        //     })
+    
+         
+    
+        //     webhook.send(args.slice(4).join(' '));
+    
+        // }
     
 }
